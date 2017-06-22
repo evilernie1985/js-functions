ae6cf61413e964d2aa065d3b7e312533360662f1
Ernest Tan <evilernie1985@gmail.com>
2017-06-22 15:28:13 +0800

    sillySum Update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 console.log("linked to my html");
+
+//fn that checks numbers with the random numbers
+function higherOrLower(guessedNum, randomNumber) {
+  if (guessedNum < randomNumber) {
+    alert("guess higher")
+  } else if (guessedNum > randomNum) {
+    alert("guess lower")
+    } else {
+    alert("correct")
+  }
+
+  }
+
+// fn that generates randomNum
+// min and max always a whole number
+// random number bewteen min and max (exclusive)
+
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min) + min)
+}
+
+//alert randomNumber
---
+++ js/sillySum.js
@@ -0,0 +1,20 @@
+// Write a function that takes an array of numbers, and returns the sum of each number multiplied by its index.
+
+// count += (number * index)
+
+
+// multiply each number by index
+// store each multiplication result in arr
+// sum elements of new array
+// function sillySum(arr) {
+//   var product;
+
+  for (i = 0; i < arr.length; i++) {
+    var productsum;
+    // var temp = []
+    productsum = (arr[i] * i) + arr[i]
+    // temp.push(product)
+
+  }
+
+console.log(productsum)
